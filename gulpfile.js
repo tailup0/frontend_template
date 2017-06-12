db880045edd0442a6e3634e1f5f4f9d5d5a0b4fa
tailup0 <onoue0527@yahoo.co.jp>
2017-06-12 17:07:31 +0900

    modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
-var browserSync = require("browser-sync");
+// var browserSync = require("browser-sync");
 var notify = require("gulp-notify");
 var pug = require("gulp-pug");
+var prettify = require('gulp-prettify')
+var csscomb = require('gulp-csscomb');
+var beautify = require('gulp-beautify');
 
-gulp.task('default', ['sass', 'browser-sync', 'pug', 'watch']);
+// gulp.task('default', ['sass', 'browser-sync', 'pug', 'watch']);
+gulp.task('default', ['sass', 'pug', 'beautify', 'watch']);
 
 //sassとpugの監視をして変換処理させる
 gulp.task('watch', () => {
@@ -15,20 +19,23 @@ gulp.task('watch', () => {
     gulp.watch(['./pug/**'], () => {
         gulp.start(['pug']);
     });
+    gulp.watch(['./js_/index.js'], () => {
+        gulp.start(['beautify']);
+    });
 });
 
 //ブラウザ表示
-gulp.task('browser-sync', () => {
-    browserSync({
-        server: {
-            baseDir: "./"   //サーバとなるrootディレクトリ
-        }
-    });
-    //ファイルの監視
-    //以下のファイルが変わったらリロードする
-	gulp.watch("./js/**/*.js",     ['reload']);
-    gulp.watch("./*.html",         ['reload']);
-});
+// gulp.task('browser-sync', () => {
+//     browserSync({
+//         server: {
+//             baseDir: "./"   //サーバとなるrootディレクトリ
+//         }
+//     });
+//     //ファイルの監視
+//     //以下のファイルが変わったらリロードする
+// 	gulp.watch("./js/**/*.js",     ['reload']);
+//     gulp.watch("./*.html",         ['reload']);
+// });
 
 //sassをcssに変換
 gulp.task("sass", () => {
@@ -37,25 +44,44 @@ gulp.task("sass", () => {
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
 		.pipe(sass())
+        .pipe(csscomb())
 		.pipe(gulp.dest("./css"))
         //reloadせずにinjectする
-        .pipe(browserSync.stream())
+        // .pipe(browserSync.stream())
 });
 
 //pugをhtmlに変換
 gulp.task("pug", () => {
-    var option = {
+    var pugOpt = {
         pretty: true
+    }
+    var prtOpt = {
+        indent_char: "\t",
+        indent_size: 1
     }
     gulp.src("./pug/**/*.pug")
         .pipe(plumber({
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
-        .pipe(pug(option))
+        .pipe(pug(pugOpt))
+        .pipe(prettify(prtOpt))
         .pipe(gulp.dest("./"))
 });
 
-//ブラウザリロード処理
-gulp.task('reload', () => {
-    browserSync.reload();
+gulp.task("beautify", () => {
+    var opt = {
+        indent_size: 1,
+        indent_with_tabs: true
+    }
+    gulp.src("./js_/index.js")
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(beautify(opt))
+        .pipe(gulp.dest("./js"))
 });
+
+//ブラウザリロード処理
+// gulp.task('reload', () => {
+//     browserSync.reload();
+// });
